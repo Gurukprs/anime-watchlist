9e5407b21c57989e03fca56551068f77e17a7b11
Guruprasaath S <gurukprs@gmail.com>
2025-11-29 19:45:56 +0530

    Batch file

--- run_anime_tracker.bat
+++ run_anime_tracker.bat
@@ -1,1 +1,16 @@
-REM placeholder - batch file content will be provided later
+@echo off
+REM Start Anime Watchlist (backend + frontend)
+
+REM Go to the folder where this .bat file is located
+cd /d "%~dp0"
+
+REM Install root dev dependency (concurrently) if node_modules not present
+IF NOT EXIST "node_modules" (
+    echo Installing root dependencies...
+    npm install
+)
+
+echo Starting backend and frontend...
+npm run dev
+
+pause
